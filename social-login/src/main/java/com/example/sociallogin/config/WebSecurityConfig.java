28227e865dbc4ec2747079dd442911f5b8d6867b
alimutlu <alimutlu_5529@icloud.com>
2022-11-05 11:13:48 +0100

    custom ouaht2 user service added.

--- social-login/src/main/java/com/example/sociallogin/config/WebSecurityConfig.java
+++ social-login/src/main/java/com/example/sociallogin/config/WebSecurityConfig.java
@@ -1,13 +1,21 @@
 package com.example.sociallogin.config;
 
+import com.example.sociallogin.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DefaultOAuth2UserService defaultOAuth2UserService;
+
+    public WebSecurityConfig(DefaultOAuth2UserService defaultOAuth2UserService) {
+        this.defaultOAuth2UserService = defaultOAuth2UserService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
